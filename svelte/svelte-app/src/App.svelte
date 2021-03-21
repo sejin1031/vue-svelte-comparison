43a5874306d9a10a2fc68ebe5f198a9102a18cf8
sejin1031 <sejin1031@utplus.co.kr>
2021-03-21 21:31:02 +0900

    svelte 스타일 적용

--- svelte/svelte-app/src/App.svelte
+++ svelte/svelte-app/src/App.svelte
@@ -1,13 +1,25 @@
-<main>
-	<div class="home">
-		<button on:click={renewal}>renewal data</button>
-		<div class="item-list-container">			
-			{#each itemList as item}
-			<Item {...item}></Item>
-			{/each}
-		</div>
+<div id="app">
+	<header>Performance comparison between Vue.js & Svelte.js</header>
+	<div id="nav">
+		<ul>
+			<li>List</li>
+			<li>Other</li>
+		</ul>
 	</div>
-</main>
+	<main>
+		<div class="home">
+			<button on:click={renewal}>renewal data</button>
+			<div class="item-list-container">			
+				{#each itemList as item}
+				<Item {...item}></Item>
+				{/each}
+			</div>
+		</div>
+	</main>
+	<footer>학사졸업논문 작성을 위한 연구용 페이지 | 작성자: 엄세진 | 한양대학교 소프웨어학부 | <a href="https://github.com/sejin1031/vue-svelte-comparison" target="_blank">Github repository</a></footer>
+</div>
+
+
 
 <script>
 	import Item from './Item.svelte'
