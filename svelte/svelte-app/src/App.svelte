18494c61cafd3d10f35e7c1a1d33bd1942cb238b
sejin1031 <sejin1031@utplus.co.kr>
2021-03-25 17:59:05 +0900

    업데이트 소요시간 추가

--- svelte/svelte-app/src/App.svelte
+++ svelte/svelte-app/src/App.svelte
@@ -14,6 +14,16 @@
 				<Item {...item}></Item>
 				{/each}
 			</div>
+			<div class="result">
+				update Start : { updateStart }
+				<br/>
+				update End : { updateEnd }
+				<br/>
+				소요시간 : { updateEnd - updateStart }
+				<br/>
+				update Avg: { updateTotal / (updateCount || 1) }
+				<br/>
+			</div>
 		</div>
 	</main>
 	<footer>학사졸업논문 작성을 위한 연구용 페이지 | 작성자: 엄세진 | 한양대학교 소프웨어학부 | <a href="https://github.com/sejin1031/vue-svelte-comparison" target="_blank">Github repository</a></footer>
@@ -23,18 +33,30 @@
 
 <script>
 	import Item from './Item.svelte'
+	import { tick } from 'svelte'
 	let itemList = new Array(10000).fill(0).map((val,index) => {
       return {
         name: 'abc' + index,
         number: 123 + '-' + index
       }
 		});
-	function renewal () {
+	let updateStart = 0;
+	let updateEnd = 0;
+	let updateCount = 0;
+	let updateTotal = 0;
+	async function renewal () {
+		updateStart = new Date().getTime()
+		updateCount = updateCount + 1;
 		itemList = new Array(10000).fill(0).map((val,index) => {
 			return {
 				name: 'd' + index,
 				number: Math.random(3)* 100 + '-' + index
 			}
 		})
+
+		await tick();
+		
+		updateEnd = new Date().getTime();
+		updateTotal += (updateEnd - updateStart)
 	}
 </script>
--- vue/compare-vue/src/views/Home.vue
+++ vue/compare-vue/src/views/Home.vue
@@ -4,6 +4,16 @@
     <div class="item-list-container">
       <Item  v-for="i in itemList" :key="i.name" :info="i"></Item>
     </div>
+    <div class="result">
+      update Start : {{ updateStart }}
+      <br/>
+      update End : {{ updateEnd }}
+      <br/>
+      소요시간 : {{ updateEnd - updateStart }}
+      <br/>
+      update Avg: {{ updateTotal / (updateCount || 1) }}
+      <br/>
+    </div>
   </div>
 </template>
 
@@ -15,25 +25,39 @@ export default {
   },
   data () {
     return {
-      itemList: []
+      itemList: [],
+      updateStart: 0,
+      updateEnd: 0,
+      updateCount: 0,
+      updateTotal: 0,
     }
   },
   beforeMount () {
     this.itemList = new Array(10000).fill(0).map((val,index) => {
-      return {
+      let obj = {
         name: '11' + index,
         number: 123 + '-' + index
       }
+      Object.freeze(obj)
+      return obj
     })
   },
   methods: {
     renewal () {
+      this.updateStart = new Date().getTime();
+      this.updateCount++;
       this.itemList = new Array(10000).fill(0).map((val,index) => {
-			return {
+        let obj = {
 				name: 'd' + index,
 				number: Math.random(3)* 100 + '-' + index
 			}
+      Object.freeze(obj)
+			return obj
 		})
+    this.$nextTick(() => {
+      this.updateEnd = new Date().getTime();
+      this.updateTotal += (this.updateEnd - this.updateStart);
+    })
     }
   }
 }
